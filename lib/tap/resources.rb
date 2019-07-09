847382daca495cebc99ac6265b053694b4b2e2fb
d0minikk <curylo.dominik@gmail.com>
2019-07-09 10:06:11 +0200

    Create resources.rb

---
+++ lib/tap/resources.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require 'tap/resources/charge'
+require 'tap/resources/token'
+require 'tap/resources/authorize'
+require 'tap/resources/card'
+require 'tap/resources/customer'
+require 'tap/resources/refund'
